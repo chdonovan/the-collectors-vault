1aec682eeefd2330d83de7912b0af6826b095918
chdonovan <chrisdonovan311@gmail.com>
2021-06-22 18:35:48 -0500

    added handlebars engine to server.js

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
+// for local server
 const express = require('express');
 const app = express();
 const port = 3001
+// for handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+// for routes
+//const routes = require('./controllers')
 
+
+
+// turns on handle bars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// test console log
 app.get('/', (req,res) => {
     res.send('Hello!')
 })
 
+// turn on routes
+//app.use(routes);
+
+// turns on local server
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
