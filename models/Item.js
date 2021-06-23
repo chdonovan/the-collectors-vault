eb513421b79d9ea7ff70e7188c4a863d775a10d3
dhartley <dani_hartley@msn.com>
2021-06-23 10:00:42 -0500

    added items model

---
+++ models/Item.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connections');
+
+class Item extends Model { }
+
+Item.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        item_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        item_description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'category',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'item',
+    }
+);
+
+module.exports = Item;
