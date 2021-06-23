6a32060211a0a2b1465cf6e8e0ba895514e3bacd
Dani Hartley <79660405+dani-hartley@users.noreply.github.com>
2021-06-23 14:25:47 -0500

    Feature/db (#11)
    * Added user model
    
    * added items model
    
    * updated Item model
    
    * added Category Model
    
    * updated Item model
    
    * addedc model associations
    
    * schema set up
    
    * added Users seeds
    
    * added Category seeds
    
    * item seed added
    
    * index for seeds added
    
    * added seed command

--- models/index.js
+++ models/index.js
@@ -1,1 +1,25 @@
-//
+//import models
+const Item = require('./Item');
+const User = require('./User');
+const Category = require('./Category');
+const { Model } = require('sequelize/types');
+
+//create associations
+Item.belongsTo(User,{
+    foreignKey:'user_id'
+});
+
+User.hasMany(Items, {
+    foreignKey: 'user_id',    
+});
+
+Item.belongsTo(Category, {
+    foreignKey: 'category_id',
+});
+
+Category.hasMany(Item, {
+    foreignKey: 'category_id',
+});
+
+//export models
+Model.exports = { Item, User, Category };
