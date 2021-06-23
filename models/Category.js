41b97415617dc4bea88548ad50443ef0941363f4
dhartley <dani_hartley@msn.com>
2021-06-23 10:06:57 -0500

    added Category Model

--- models/Category.js
+++ models/Category.js
@@ -1,2 +1,28 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connections');
+
+class Category extends Model {}
+
+Category.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        category_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTablesName: true,
+        underscored: true,
+        modelName: 'category'
+    }
+);
+
+module.exports = Category;
