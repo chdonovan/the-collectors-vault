0f950e5569390bfc7f484b223d94b65bda33f9f9
jose <jmolina721@gmail.com>
2021-06-23 19:37:56 -0500

    Created more user routes

---
+++ controllers/api/item-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const { Item } = require('../../models');
+
+// GET All Items /api/collections
+router.get('/', (req, res) => {
+    Item.findAll({
+        
+        })
+        .then(dbItemData => res.json(dbItemData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// GET Item by ID
+router.get('/:id', (req, res) => {
+    Item.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+});
