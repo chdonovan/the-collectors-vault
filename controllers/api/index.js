ca11030b5182acdd93d9ae37677b808b75b2b0e1
chdonovan <chrisdonovan311@gmail.com>
2021-06-25 18:43:07 -0500

    Category routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const itemRoutes = require('./item-routes.js');
+const categoryRoutes = require('./category-routes');
 
 router.use('/users', userRoutes);
 router.use('/items', itemRoutes);
+router.use('/categories', categoryRoutes);
 
 module.exports = router;
