58dcec37d26e53625a811e961257aa637803171b
chdonovan <chrisdonovan311@gmail.com>
2021-06-26 13:18:16 -0500

    logout logic added/ button added to main.handlebars// logout route added to user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -93,6 +93,17 @@ router.post('/login', (req, res) => {
       });
     });
 });
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+});
+  
 // PUT Update Users by ID /api/users/1
 router.put('/:id', (req, res) => {
     User.update(req.body, {
