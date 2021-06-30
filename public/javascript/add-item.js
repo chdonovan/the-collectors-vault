ad0a0547a93a66e17b29060cbfebd65a9479e569
dhartley <dani_hartley@msn.com>
2021-06-30 10:48:54 -0500

    updated add-item

--- public/javascript/add-item.js
+++ public/javascript/add-item.js
@@ -1,16 +1,21 @@
+
 async function newFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="item-title"]').value;
     const item_description = document.querySelector('input[name="item-text"]').value;
     const inventory = document.querySelector('input[name="item-inventory"]').value;
+    // const user_id = req.session.user_id;
+    const category_id = document.querySelector('select[name="category"]').value.toString().split('/')[0];
 
-    const response = await fetch(`/api/post`, {
+    const response = await fetch(`/api/categories`, {
         method: 'POST',
         body: JSON.stringify({
             title,
             item_description,
-            inventory
+            inventory,
+            user_id: req.session.user_id,
+            category_id
         }),
         headers: {
             'Content-Type': 'application/json'
